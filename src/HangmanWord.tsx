3db4698092bbabf8ffc191e5511ea3615ee6e57e
Sarah Zwart <sarahczwart@gmail.com>
2024-02-03 11:54:50 -0500

    HangWord style

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,17 @@
 export function HangmanWord(){
+    return (
+    <div 
+    style = {{ 
+        display: "flex", 
+        gap: ".25em", 
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace"
+        }}
+    >
+        
+    </div>
+    )
 
 }
