b95aa82fa16dea06cc8014ea3f0396fe2dea1827
Sarah Zwart <sarahczwart@gmail.com>
2024-02-03 13:06:29 -0500

    functionality

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -3,7 +3,7 @@ type HangmanWordProps = {
     wordToGuess: string
 }
 
-export function HangmanWord({guessedLetters, wordToGuess}){
+export function HangmanWord({guessedLetters, wordToGuess}: HangmanWordProps){
     return (
     <div 
     style = {{ 
